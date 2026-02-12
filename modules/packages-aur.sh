30ba3d26812ab795574d1692252e91e7b76c9478
zen0x00 <amanchaitany@proton.me>
2026-02-12 15:50:09 +0530

    updated packages and binds

--- modules/packages-aur.sh
+++ modules/packages-aur.sh
@@ -4,7 +4,7 @@ install_aur_packages() {
     local packages=(
         impala tuned hyprsunset tuned-ppd wiremix rofi-wayland bluetui hyprland xdg-desktop-portal-hyprland kitty awww-git hyprlock swaync
         waybar quickshell fastfetch zsh stow bc jq ffmpeg imagemagick yad hypridle wlogout
-        notify-send thunar nwg-look steam neovim openrgb fzf lazygit sddm starship treesitter-cli ani-cli
+        notify-send nautilus nwg-look steam neovim openrgb fzf lazygit sddm starship treesitter-cli ani-cli
         sddm-silent-theme zoxide ttf-jetbrains-mono-nerd gamemode lib32-gamemode localsend idescriptor-git
         ttf-ibm-plex ttf-roboto noto-fonts spotify spicetify-cli mangohud ttf-gohu-nerd sunset-cursors-git
         zen-browser-bin ttf-twemoji discord hyprshot obs-studio obs-vkcapture obs-pipewire-audio-capture swayosd abyssal-gtk-theme gtk-engine-murrine
